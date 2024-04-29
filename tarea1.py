8951c5fb0b69bff579efe7227c7260f7f34eca8a
JoseChristiansen <155940973+JoseChristiansen@users.noreply.github.com>
2024-04-29 05:52:43 -0400

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,5 @@
+harina = int(input("introduzca la cantidad de gramos de harina que posee"))
+agua = int(input("introduzca la cantidad de gramos de agua que posee"))
+sal = int(input("introduzca la cantidad de gramos de sal que posee"))
+masa_total = harina + agua + sal
+print(f"la cantidad en gramos de masa de arepa que poseeras es de {masa_total}")
